3c11cf6c2b03467e4c6077e32baab77bc7624123
粱恩珍 <jean@YangMacBook-Pro.local>
2020-10-27 18:21:51 +0900

    Creat MemoListScrren

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/elements/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
+      <MemoListScreen />
     </View>
   );
 }
@@ -22,5 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 78,
   },
-
 });
