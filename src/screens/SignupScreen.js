f62317b56f524b33c9d7fedf43c7d57a2fa2c798
粱恩珍 <jean@YangMacBook-Pro.local>
2020-10-28 19:23:17 +0900

    Creat Login and Signup

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  StyleSheet, View, Text, TextInput,
+  TouchableHighlight,
+} from 'react-native';
+
+class SignupScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          メンバー登録
+        </Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <TouchableHighlight style={styles.button} onPress={() => {}} underlayColor="#823d81">
+          <Text style={styles.buttonTitle}>登録する</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 8,
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#e371d4',
+    height: 48,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    alignSelf: 'center',
+  },
+  buttonTitle: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default SignupScreen;
